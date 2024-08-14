ce5d8cf6719c0f05f8dacbaef8159050af217bff
Lucas Gama <lucasgdon@gmail.com>
2024-08-14 02:19:44 -0300

    Ajuste de migration para delete em cascade

--- database/migrations/2024_08_13_042719_create_task_user_table.php
+++ database/migrations/2024_08_13_042719_create_task_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('task_user', function (Blueprint $table) {
-            $table->foreignId('task_id')->constrained();
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
