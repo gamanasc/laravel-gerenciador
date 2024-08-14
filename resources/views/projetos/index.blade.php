4bbc7091c7d8bd8e7b8691c2d0ce7cf6e1865ab2
Lucas Gama <lucasgdon@gmail.com>
2024-08-13 21:31:58 -0300

    Inclusão de classes e botões de componente criados pelo breeze na listagem de projetos

--- resources/views/projetos/index.blade.php
+++ resources/views/projetos/index.blade.php
@@ -6,56 +6,67 @@
                 {{ __('Projetos') }}
             </h2>
 
-            <a href="{{ route('projetos.create') }}" class="btn btn-primary">Adicionar</a>
+            <a href="{{ route('projetos.create') }}" >
+                <x-primary-button class="mx-1">
+                    {{ __('Adicionar') }}
+                </x-primary-button>
+            </a>
         </div>
 
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @isset($mensagemSucesso)
+            <div class="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300 mb-4" role="alert">
+                <span class="font-medium">{{ $mensagemSucesso }}</span>
+            </div>
+            @endisset
+
+            @isset($mensagemErro)
+            <div class="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300 mb-4">
+                <span class="font-medium">{{ $mensagemErro }}</span>
+            </div>
+            @endisset
+
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                    <ul class="divide-y divide-gray-200 p-0 m-0">
+                        @foreach ($projetos as $projeto)
+                            <li class="flex justify-between items-center p-4">
+                                <span class="text-sm">
+                                    <span class="text-lg font-semibold text-gray-900">{{ $projeto->titulo }}</span><br>
+                                    <small class="text-gray-600">{{ $projeto->descricao }}</small>
+                                </span>
 
+                                <span class="flex space-x-2">
 
-                @isset($mensagemSucesso)
-                <div class="alert alert-success">
-                    {{ $mensagemSucesso }}
+                                    <a href="{{ route('projetos.show', $projeto->id) }}">
+                                        <x-secondary-button class="mx-1">
+                                            {{ __('Ver') }}
+                                        </x-secondary-button>
+                                    </a>
+
+                                    <a href="{{ route('projetos.edit', $projeto->id) }}">
+                                        <x-primary-button class="mx-1">
+                                            {{ __('Editar') }}
+                                        </x-primary-button>
+                                    </a>
+
+                                    <form action="{{ route('projetos.destroy', $projeto->id) }}" method="post">
+                                        @csrf
+
+                                        <x-danger-button class="mx-1">
+                                            {{ __('Remover') }}
+                                        </x-danger-button>
+                                    </form>
+                                </span>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
-                @endisset
 
-                @isset($mensagemErro)
-                <div class="alert alert-danger">
-                    {{ $mensagemErro }}
-                </div>
-                @endisset
-
-                <ul class="list-group">
-                    @foreach ($projetos as $projeto)
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            <span>
-                                {{$projeto->titulo}} <br>
-                                <small>{{$projeto->descricao}}</small>
-                            </span>
-
-                            <span class="d-flex">
-
-                                <a href="{{ route('projetos.show', $projeto->id) }}" class="btn btn-dark btn-sm">
-                                    Ver
-                                </a>
-
-                                <a href="{{ route('projetos.edit', $projeto->id) }}" class="btn btn-primary btn-sm mx-1">
-                                    Editar
-                                </a>
-
-                                <form action="{{ route('projetos.destroy', $projeto->id) }}" method="post">
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        Remover
-                                    </button>
-                                </form>
-                            </span>
-                        </li>
-                    @endforeach
-                </ul>
 
             </div>
         </div>
